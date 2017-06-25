b826acd6c51e3fc77e808d798b057cda2ebea333
Gorka <gorka@aquigorka.com>
2017-06-25 09:17:33 +0200

    Prototype Excercise: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Prototype Exercise
+
+## Admin
+
+## Mobile
---
+++ data.js
@@ -0,0 +1,27 @@
+module.exports = {
+  types: [
+    { id: '1', label: 'Samsung Audio', ip: '', controls: [
+      { id: 'button', label: 'Power' },
+      { id: 'slider', label: 'Volume' },
+      { id: 'select', label: 'Playlist', data: ['Samsung Audio 1', 'Samsung Audio 2'] },
+    ]},
+    { id: '2', label: 'Sony Audio', ip: '', controls: [
+      { id: 'button', label: 'Power' },
+      { id: 'slider', label: 'Volume' },
+      { id: 'select', label: 'Playlist', data: ['Sony Aduio 1', 'Sony Audio 2'] },
+    ]},
+    { id: '3', label: 'Apple TV', ip: '', controls: [
+      { id: 'button', label: 'Power' },
+      { id: 'slider', label: 'Brightness' },
+      { id: 'slider', label: 'Volume' },
+    ]},
+    { id: '4', label: 'Citrus Lights', ip: '', controls: [
+      { id: 'button', label: 'On/Off' },
+    ]},
+  ],
+  devices: [
+    { id: 'bedroom-apple-tv', label: 'Bedroom Apple TV', type: '3', api: { verb: '', url: '' }, state: [ 0,0,0 ]},
+    { id: 'livingroom-player', label: 'Livingroom Player', type: '1', api: { verb: '', url: '' }, state: [ 0,0,0 ]},
+    { id: 'livingroom-lights', label: 'Livingroom Lights', type: '4', api: { verb: '', url: '' }, state: [ 0 ]},
+  ],
+}
