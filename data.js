c5367fae83e64e6c69441b1d1e80c9b946b61178
Gorka <gorka@aquigorka.com>
2017-06-26 19:53:56 +0200

    Updates default data ids to improve sorting

--- data.js
+++ data.js
@@ -1,27 +1,27 @@
 export default {
   types: [
-    { id: '1', label: 'Samsung Audio', controls: [
-      { type: 'button', label: 'Power', id: '1', },
-      { type: 'slider', label: 'Volume', id: '2', },
-      { type: 'select', label: 'Playlist', id: '3', data: ['Samsung Audio 1', 'Samsung Audio 2'], },
+    { id: '001', label: 'Samsung Audio', controls: [
+      { type: 'button', label: 'Power', id: '001', },
+      { type: 'slider', label: 'Volume', id: '002', },
+      { type: 'select', label: 'Playlist', id: '003', data: ['Samsung Audio 1', 'Samsung Audio 2'], },
     ]},
-    { id: '2', label: 'Sony Audio', controls: [
-      { type: 'button', label: 'Power', id: '1', },
-      { type: 'slider', label: 'Volume', id: '2', },
-      { type: 'select', label: 'Playlist', id: '3', data: ['Sony Audio 1', 'Sony Audio 2'], },
+    { id: '002', label: 'Sony Audio', controls: [
+      { type: 'button', label: 'Power', id: '001', },
+      { type: 'slider', label: 'Volume', id: '002', },
+      { type: 'select', label: 'Playlist', id: '003', data: ['Sony Audio 1', 'Sony Audio 2'], },
     ]},
-    { id: '3', label: 'Apple TV', controls: [
-      { type: 'button', label: 'Power', id: '1', },
-      { type: 'slider', label: 'Brightness', id: '2', },
-      { type: 'slider', label: 'Volume', id: '3', },
+    { id: '003', label: 'Apple TV', controls: [
+      { type: 'button', label: 'Power', id: '001', },
+      { type: 'slider', label: 'Brightness', id: '002', },
+      { type: 'slider', label: 'Volume', id: '003', },
     ]},
-    { id: '4', label: 'Citrus Lights', controls: [
-      { type: 'button', label: 'On/Off', id: '1', },
+    { id: '004', label: 'Citrus Lights', controls: [
+      { type: 'button', label: 'On/Off', id: '001', },
     ]},
   ],
   devices: [
-    { id: '001', label: 'Bedroom Apple TV', type: '3', api: { verb: '', url: '' }, state: [ { id: '1', value: 0 }, { id: '2', value: 0 }, { id: '3', value: 0 } ]},
-    { id: '002', label: 'Livingroom Player', type: '1', api: { verb: '', url: '' }, state: [ { id: '1', value: 0 }, { id: '2', value: 0 }, { id: '3', value: 0 } ]},
-    { id: '003', label: 'Livingroom Lights', type: '4', api: { verb: '', url: '' }, state: [ { id: '1', value: 0 } ]},
+    { id: '001', label: 'Bedroom Apple TV', type: '003', api: { verb: '', url: '' }, state: [ { id: '001', value: 0 }, { id: '002', value: 0 }, { id: '003', value: 0 } ]},
+    { id: '002', label: 'Livingroom Player', type: '001', api: { verb: '', url: '' }, state: [ { id: '001', value: 0 }, { id: '002', value: 0 }, { id: '003', value: 0 } ]},
+    { id: '003', label: 'Livingroom Lights', type: '004', api: { verb: '', url: '' }, state: [ { id: '001', value: 0 } ]},
   ],
 }
