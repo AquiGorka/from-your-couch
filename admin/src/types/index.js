a8db527b9fbeea84182b4e5d08593854fca92e7c
Gorka <gorka@aquigorka.com>
2017-06-26 16:59:07 +0200

    Adds ux visuals to configure types section

--- admin/src/types/index.js
+++ admin/src/types/index.js
@@ -1,5 +1,10 @@
 import React, { PureComponent } from 'react'
 import './styles.css'
+import { Card, CardTitle, CardText } from 'react-toolbox/lib/card';
+import Input from 'react-toolbox/lib/input';
+import { Layout, Panel, Sidebar } from 'react-toolbox';
+import Dropdown from 'react-toolbox/lib/dropdown';
+import { Button } from 'react-toolbox/lib/button';
 
 const controls = [
   { id: 'button', label: 'Button' },
@@ -10,25 +15,17 @@ const controls = [
 class Types extends PureComponent {
 
   // controls
-  onAddControl = (typeId) => {
-    // default adds button and then it is "modifyable"
+  onAddControl = (modifiedType) => {
+    console.log('onaddcontrol: ', modifiedType)
     const { types = [], ...rest } = this.props.data
-    const { controls = [], ...other } = types.find(x => x.id === typeId)
-    const newId = `${Date.now()}`
-    const newControl = {
-      id: newId,
-      label: 'New Control',
-      type: 'button',
-    }
-    const newControls = controls.concat([newControl])
-    const newTypes =  types.filter(x => x.id !== typeId)
-      .concat([{ controls: newControls, ...other }])
+    const newTypes = types.filter(x => x.id !== modifiedType.id)
+      .concat(modifiedType)
     // update states
     // new state id: controlId, value: 0
     const { devices = [], ...another } = rest
     // filter all devices that use the type
-    const needToUpdate = devices.filter(device => device.type === typeId)
-    const noNeedToUpdate = devices.filter(device => device.type !== typeId)
+    const needToUpdate = devices.filter(device => device.type === modifiedType.id)
+    const noNeedToUpdate = devices.filter(device => device.type !== modifiedType.id)
     // new state
     const updatedDevices = needToUpdate.map(item => {
       const { state = [], ...rest } = item
@@ -93,22 +90,16 @@ class Types extends PureComponent {
   }
 
   // types
-  onAdd = () => {
+  onAdd = (newType) => {
     const { types = [], ...rest } = this.props.data
-    const newType = {
-      id: `${Date.now()}`,
-      label: this.refs.label.value,
-    }
     const newTypes = types.concat([newType])
     this.props.onUpdate({ types: newTypes, ...rest })
   }
 
-  onUpdate = (id) => {
+  onUpdate = (modifiedType) => {
     const { types, ...rest } = this.props.data
-    const type = types.find(x => x.id === id)
-    const { label, ...other } = type
-    const modifiedType = { label: this.refs[`label_${id}`].value, ...other }
-    const newTypes = types.filter(x => x.id !== id).concat([modifiedType]) 
+    const newTypes = types.filter(x => x.id !== modifiedType.id)
+      .concat([modifiedType]) 
     this.props.onUpdate({ types: newTypes,  ...rest })
   }
 
@@ -132,37 +123,152 @@ class Types extends PureComponent {
     const { types = [] } = data
     return (
       <div>
-        <div>
-          <input type="text" ref="label" />
-          <input type="button" onClick={this.onAdd} value="Add" />
-        </div>
-        <ul>
-          {types.sort((a, b) => a.id > b.id).map((item, index) => 
-            <li key={item.id}>
-              <div onClick={() => {
-                this.onDelete(item.id)
-              }}>-</div>  
-              <div onClick={() => {
-                this.onUpdate(item.id)
-              }}>o</div>
-              <div onClick={() => {
-                this.onAddControl(item.id)
-              }}>a</div>
-              <div>
-                <input ref={`label_${item.id}`} type="text" defaultValue={item.label} />
+        <Layout>
+          <Panel className="panel-fix">
+            {types.sort((a, b) => a.id > b.id).map(item => 
+              <div key={`type_${item.id}`}>
+                <DeviceType
+                  item={item}
+                  onDelete={this.onDelete}
+                  onUpdate={this.onUpdate}
+                  onDeleteControl={this.onDeleteControl}
+                  onUpdateControl={this.onUpdateControl} />
               </div>
+            )}
+          </Panel>
+          <Sidebar pinned={true}>
+            <Card>
+              <CardTitle title="Actions" />
+            </Card>
+            <NewDeviceType onAdd={this.onAdd} />
+            <NewDeviceTypeControl
+              types={types}
+              onAdd={this.onAddControl} />
+            <NewDeviceTypeControlData
+              types={types}
+              onUpdate={this.onUpdateControl} />
+          </Sidebar>
+        </Layout>
+      </div>    
+    )
+  }
+}
+
+class NewDeviceTypeControl extends PureComponent {
+  
+  state = { typeId: -1 }
+  
+  onAdd = () => {
+    // default label
+    if (this.state.typeId === -1) {
+      return
+    }
+    // default adds button and then it is "modifyable"
+    const { onAdd, types } = this.props
+    const type = types.find(x => x.id === this.state.typeId)
+    const { controls = [], ...rest  } = type
+    const newControl = {
+      id: `${Date.now()}`,
+      label: 'Control name',
+      type: 'button'
+    }
+    const newControls = controls.concat([newControl])
+    const modifiedType = { controls: newControls, ...rest }
+    onAdd(modifiedType)
+  }
+  
+  render() {
+    const { types = [] } = this.props
+    const data = [{ id: -1, label: 'Choose to which Device Type'}]
+      .concat(types).map(x => {
+        return { label: x.label, value: x.id }})
+    return (
+      <Card>
+        <CardTitle subtitle="Add new Control" />
+        <CardText>
+          <select ref="select" defaultValue={-1} onChange={() => {
+              this.setState({ typeId: this.refs.select.value })
+            }}>
+            {data.map(item => {
+              return <option key={`new_control_${item.value}`} value={item.value}>{item.label}</option>
+            })}
+          </select>
+          <Button label="Add" onClick={this.onAdd} />
+        </CardText>
+      </Card>
+    ) 
+  }
+}
+
+class DeviceType extends PureComponent {
+
+  onUpdate = () => {
+    const { item, onUpdate } = this.props
+    const { id } = item
+    const { label, ...rest } = item
+    const newLabel =this.refs[`label_${id}`].value
+    const modifiedType = {
+      label: newLabel, ...rest}
+    onUpdate(modifiedType)
+  }
+
+  render() {
+    const {
+      item,
+      onDeleteControl,
+      onUpdateControl,
+      onUpdate,
+      onDelete
+    } = this.props
+    const { id, label = '', controls = [] } = item
+    return (
+        <Card className="type-card">
+          <CardTitle subtitle={label} />
+          <CardText>
+            <div className="wrapper">
+              <div className="delete" onClick={() => { onDelete(id) }}>x</div>  
+              <input
+                ref={`label_${id}`}
+                type="text"
+                defaultValue={label}
+                onChange={this.onUpdate} />
               <ControlsList
-                controls={item.controls}
+                controls={controls}
                 onDelete={(controlId) => {
-                  this.onDeleteControl(item.id, controlId)
+                  onDeleteControl(id, controlId)
                 }}
                 onUpdate={(controlId, newControl) => {
-                  this.onUpdateControl(item.id, controlId, newControl)
+                  onUpdateControl(id, controlId, newControl)
                 }} />
-            </li>
-          )}
-        </ul>
-      </div>
+            </div>
+          </CardText>
+        </Card>
+    )
+  }
+}
+
+class NewDeviceType extends PureComponent {
+  state = { label: '' }
+
+  onAdd = () => {
+    const newType = {
+      id: `${Date.now()}`,
+      label: this.refs.input.value,
+    }
+    this.props.onAdd(newType)
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardTitle subtitle="Add new Device Type" />
+        <CardText>
+          <div>
+            <input type="text" placeholder="New device type name" ref="input" />
+          </div>
+          <Button label="Add" onClick={this.onAdd} />
+        </CardText>
+      </Card>
     )
   }
 }
@@ -172,24 +278,23 @@ class ControlsList extends PureComponent {
   render() {
     const { controls = [] } = this.props
     return (
-      <ul>
+      <ul className="control-list">
         {controls.sort((a, b) => a.id > b.id).map(item => 
-          <li key={`control_${item.id}`}>
-            <div onClick={() => {
-              this.props.onDelete(item.id)
-            }}>-</div>  
-            <div onClick={() => {
-              const { label, ...rest } = item
-              const newLabel = this.refs[`control_${item.id}_label`].value
-              this.props.onUpdate(item.id, { label: newLabel, ...rest })
-            }}>o</div>
-            <div>
-              <input ref={`control_${item.id}_label`} type="text" defaultValue={item.label} />
-              <ControlPicker control={item} onUpdate={(newControlType) => {
-                const { type, ...rest } = item
-                this.props.onUpdate(item.id, { type: newControlType, ...rest })
-              }} />
-            </div>
+          <li className="control-item" key={`control_${item.id}`}>
+            <div className="delete-control" onClick={() => { this.props.onDelete(item.id) }}>x</div>  
+            <input
+              onChange={() => {
+                const { label, ...rest } = item
+                const newLabel = this.refs[`control_${item.id}_label`].value
+                this.props.onUpdate(item.id, { label: newLabel, ...rest })
+              }}
+              ref={`control_${item.id}_label`}
+              type="text"
+              defaultValue={item.label} />
+            <ControlPicker control={item} onUpdate={(newControlType) => {
+              const { type, ...rest } = item
+              this.props.onUpdate(item.id, { type: newControlType, ...rest })
+            }} />
             <ControlDatalist
               type={item.type}
               data={item.data}
@@ -208,18 +313,16 @@ class ControlPicker extends PureComponent {
   render() {
     const { control } = this.props
     return (
-      <div>
-        <select
-          onChange={() => {
-            this.props.onUpdate(this.refs[`control_${control.id}_select`].value)
-          }}
-          ref={`control_${control.id}_select`}
-          defaultValue={control.type}>
-            {controls.map((item, index) => 
-              <option key={index} value={item.id}>{item.label}</option>
-          )}
-        </select>
-      </div>
+      <select
+        onChange={() => {
+          this.props.onUpdate(this.refs[`control_${control.id}_select`].value)
+        }}
+        ref={`control_${control.id}_select`}
+        defaultValue={control.type}>
+          {controls.map((item, index) => 
+            <option key={index} value={item.id}>{item.label}</option>
+        )}
+      </select>
     )
   }
 }
@@ -231,28 +334,85 @@ class ControlDatalist extends PureComponent {
       return <div></div>
     }
     return (
-      <div>
-        <ul>
-          {data.map((item, index) =>
-            <li key={`data_${item}_${index}`}>
-              <div onClick={() => {
-                const newData = data.filter((x, i) => i !== index)
-                onUpdate(newData)
-              }}>-</div>
-              <div><input ref="label" type="text" defaultValue={item} /></div>
-            </li>
-          )}
-        </ul>
-        <div>
-          <input type="text" ref="new" />
-          <input type="button" value="Add" onClick={() => {
-            const newLabel = this.refs.new.value
-            const newData = data.concat([newLabel])
-            onUpdate(newData)
-          }} />
-        </div>
-      </div>
+      <ul className="control-data-list">
+        {data.map((item, index) =>
+          <li className="control-data-item" key={`data_${item}_${index}`}>
+            <div className="control-data-delete" onClick={() => {
+              const newData = data.filter((x, i) => i !== index)
+              onUpdate(newData)
+            }}>x</div>
+            <div><input ref="label" type="text" defaultValue={item} /></div>
+          </li>
+        )}
+      </ul>
     )
+  }
+}
+
+class NewDeviceTypeControlData extends PureComponent {
+  
+  state = { typeId: -1, controlId: -1 }
+  
+  onAdd = () => {
+    // default labels
+    if (this.state.typeId === -1) {
+      alert('Please select a Device Type from the list')
+      return
+    }
+    if (this.state.controlId === -1) {
+      alert('Please select a Control from the list')
+      return
+    }
+    const { onUpdate, types } = this.props
+    const type = types.find(x => x.id === this.state.typeId)
+    const { controls, ...rest } = type
+    const control = controls.find(x => x.id === this.state.controlId)
+    const { data = [], ...other } = control
+    const newData = data.concat([this.refs.input.value])
+    const newControl = { data: newData, ...other }
+    const newControls = controls.filter(x => x.id === this.state.controlId)
+        .concat([newControl])
+    const newType = { controls: newControls, ...other }
+    // TODO refactor onUpdateControl to receive the correct signature
+    //onUpdate(newType)
+    onUpdate(this.state.typeId, this.state.controlId, newControl)
+  }
+  
+  render() {
+    const { types = [] } = this.props
+    const data = [{ id: -1, label: 'Choose to which Device Type'}]
+      .concat(types).map(x => {
+        return { label: x.label, value: x.id }})
+    const type = types.find(x => x.id === this.state.typeId) || {}
+    const { controls = [] } = type
+    const controlsData = [{ id: -1, label: 'Choose to which Select Control'}]
+      .concat(controls.filter(x => x.type === 'select'))
+        .map(x => { return { value: x.id, label: x.label }})
+    return (
+      <Card>
+        <CardTitle subtitle="Add new item to list" />
+        <CardText className="data-add-item-wrapper">
+          <select ref="select" defaultValue={-1} onChange={() => {
+              this.setState({ typeId: this.refs.select.value })
+            }}>
+            {data.map(item => {
+              return <option key={`new_control_${item.value}`} value={item.value}>{item.label}</option>
+            })}
+          </select>
+          <select ref="select_control" defaultValue={-1} onChange={() => {
+              this.setState({ controlId: this.refs.select_control.value })
+            }}>
+            {controlsData.map(item => {
+              return <option key={`new_control_${item.value}`} value={item.value}>{item.label}</option>
+            })}
+          </select>
+          <div>
+            <input type="text" ref="input" placeholder="New item" />
+          </div>
+          <Button label="Add" onClick={this.onAdd} />
+        </CardText>
+      </Card>
+    ) 
   }
 }
 
--- data.js
+++ data.js
@@ -1,27 +1,27 @@
 export default {
   types: [
     { id: '1', label: 'Samsung Audio', controls: [
-      { type: 'button', label: 'Power', id: 1, },
-      { type: 'slider', label: 'Volume', id: 2, },
-      { type: 'select', label: 'Playlist', id: 3, data: ['Samsung Audio 1', 'Samsung Audio 2'], },
+      { type: 'button', label: 'Power', id: '1', },
+      { type: 'slider', label: 'Volume', id: '2', },
+      { type: 'select', label: 'Playlist', id: '3', data: ['Samsung Audio 1', 'Samsung Audio 2'], },
     ]},
     { id: '2', label: 'Sony Audio', controls: [
-      { type: 'button', label: 'Power', id: 1, },
-      { type: 'slider', label: 'Volume', id: 2, },
-      { type: 'select', label: 'Playlist', id: 3, data: ['Sony Audio 1', 'Sony Audio 2'], },
+      { type: 'button', label: 'Power', id: '1', },
+      { type: 'slider', label: 'Volume', id: '2', },
+      { type: 'select', label: 'Playlist', id: '3', data: ['Sony Audio 1', 'Sony Audio 2'], },
     ]},
     { id: '3', label: 'Apple TV', controls: [
-      { type: 'button', label: 'Power', id: 1, },
-      { type: 'slider', label: 'Brightness', id: 2, },
-      { type: 'slider', label: 'Volume', id: 3, },
+      { type: 'button', label: 'Power', id: '1', },
+      { type: 'slider', label: 'Brightness', id: '2', },
+      { type: 'slider', label: 'Volume', id: '3', },
     ]},
     { id: '4', label: 'Citrus Lights', controls: [
-      { type: 'button', label: 'On/Off', id: 1, },
+      { type: 'button', label: 'On/Off', id: '1', },
     ]},
   ],
   devices: [
-    { id: '1', label: 'Bedroom Apple TV', type: '3', api: { verb: '', url: '' }, state: [ { id: 1, value: 0 }, { id: 2, value: 0 }, { id: 3, value: 0 } ]},
-    { id: '2', label: 'Livingroom Player', type: '1', api: { verb: '', url: '' }, state: [ { id: 1, value: 0 }, { id: 2, value: 0 }, { id: 3, value: 0 } ]},
-    { id: '3', label: 'Livingroom Lights', type: '4', api: { verb: '', url: '' }, state: [ { id: 1, value: 0 } ]},
+    { id: '1', label: 'Bedroom Apple TV', type: '3', api: { verb: '', url: '' }, state: [ { id: '1', value: 0 }, { id: '2', value: 0 }, { id: '3', value: 0 } ]},
+    { id: '2', label: 'Livingroom Player', type: '1', api: { verb: '', url: '' }, state: [ { id: '1', value: 0 }, { id: '2', value: 0 }, { id: '3', value: 0 } ]},
+    { id: '3', label: 'Livingroom Lights', type: '4', api: { verb: '', url: '' }, state: [ { id: '1', value: 0 } ]},
   ],
 }
