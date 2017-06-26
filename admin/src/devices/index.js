443d328ca312fc995084fe5937fa6fcb522f2025
Gorka <gorka@aquigorka.com>
2017-06-26 18:23:13 +0200

    Fixes key missing

--- admin/src/devices/index.js
+++ admin/src/devices/index.js
@@ -162,7 +162,7 @@ class NewDevice extends PureComponent {
               }}
               ref="select">
               {types.map(item => {
-                return <option value={item.id}>{item.label}</option>                   
+                return <option key={item.id} value={item.id}>{item.label}</option>                   
               })}
             </select>
           </div>
