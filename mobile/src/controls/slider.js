02eb7ea989e361eccdaa9af494c62753eeff09bf
Gorka <gorka@aquigorka.com>
2017-06-26 09:47:55 +0200

    Adds ux visuals to slider control

--- mobile/src/controls/slider.js
+++ mobile/src/controls/slider.js
@@ -1,23 +1,19 @@
 import React, { PureComponent } from 'react'
+import Slider from 'react-toolbox/lib/slider';
 
 class Button extends PureComponent {
   render() {
     const { state, onUpate } = this.props
     const { id, value } = state
     return (
-      <div>
-        <input
-          ref="input"
-          type="range"
-          min="0"
-          max="100"
-          step="1"
-          defaultValue={value}
-          onChange={() => {
-            this.props.onUpdate({ id, value: this.refs.input.value | 0 })
-          }}
-        />
-      </div>
+      <Slider
+        value={value}
+        min={0}
+        max={100}
+        step={1}
+        onChange={value => {
+          this.props.onUpdate({ id, value })
+        }} />
     )
   }
 }
